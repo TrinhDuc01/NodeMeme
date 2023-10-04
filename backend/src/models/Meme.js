73706e3328db755638b2cac525d8cdd86ed9eaa2
TrinhDuc01 <ductrinh.0301@gmail.com>
2023-10-04 12:32:35 +0700

    fix association n-1

--- backend/src/models/Meme.js
+++ backend/src/models/Meme.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
-            Meme.hasOne(models.Category);
+            Meme.belongsTo(models.Category);
         }
     };
     Meme.init({
