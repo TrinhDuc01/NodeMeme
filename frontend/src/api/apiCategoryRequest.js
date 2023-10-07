b32a096fbedf7e4a38969ff2d0ea8c27600c05f8
TrinhDuc01 <ductrinh.0301@gmail.com>
2023-10-08 00:39:22 +0700

    api req create category

---
+++ frontend/src/api/apiCategoryRequest.js
@@ -0,0 +1,18 @@
+import { toast } from "react-toastify";
+import { axiosAuth } from "./config";
+
+export const createCategory = async (category, naviagte, accessToken, axiosRefeshToken) => {
+    try {
+        const res = await axiosRefeshToken.post("api/category/create", category, {
+            headers: {
+                token: `Bearer ${accessToken}`,
+            }
+        });
+        toast.success(res.data.message);
+        naviagte('/admin/category')
+    } catch (error) {
+        // neu res 40x thi no chay vao day, lay response tu error
+        toast.error(error.response?.data.message)
+        console.log(error);
+    }
+}
