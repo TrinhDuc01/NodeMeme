79a7ffbcf360500ebd3bd8228689cbc036cca109
TrinhDuc01 <ductrinh.0301@gmail.com>
2023-10-08 00:41:48 +0700

    fronten chuc nang them category

--- frontend/src/pages/CategoryManage.js
+++ frontend/src/pages/CategoryManage.js
@@ -1,20 +1,54 @@
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router'
 import jwt_decode from "jwt-decode";
+import Form from '../components/Form';
+import Input from '../components/Input';
+import Button from '../components/Button';
+import { createCategory } from '../api/apiCategoryRequest';
+import { createAxiosRefreshToken } from '../api/apiRefreshToken';
+import { loginSuccess } from '../redux/authSlice';
 
 
 export default function CategoryManage() {
-    
-    const naviagte = useNavigate();
     const user = useSelector(state => state.auth.login.currentUser);
-    const decode = jwt_decode(user?.accessToken)// giai ma lay payload cua access token
-    
+    const [categoryName, setCategoryName] = useState('')
+    const naviagte = useNavigate();
+    const dispatch = useDispatch();
+    let axiosRefeshToken = createAxiosRefreshToken(user?.accessToken, dispatch, loginSuccess); // tra ve 1 axios kiem tra access token
+    console.log(user?.accessToken)
+
+    let decode;
+    if (user) decode = jwt_decode(user?.accessToken)// giai ma lay payload cua access token
+
     useEffect(() => {
         if (!decode.isadmin) naviagte('/') // khong phai admin thi ve trang chu
     }, [])
-    
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const category = {
+            name: categoryName,
+            isActive: 1
+        }
+
+        createCategory(category, naviagte, user?.accessToken, axiosRefeshToken);//tao 1 category 
+        setCategoryName('')
+    }
+
     return (
-        <div>CategoryManage</div>
+        <div className='row mt-lg-1 d-block d-lg-flex'>
+            <div className='col-3'></div>
+            <div className='col-lg-6 mt-lg-1'>
+                <Form handleSearchSubmit={handleSubmit} className='d-grid gap-2'>
+                    <Input handleOnchange={e => setCategoryName(e.target.value)} value={categoryName}
+                        className='form-control me-2'
+                        name='username' type="text" placeholder="Category Name" />
+                    <div className='text-center'>
+                        <Button className="btn btn-primary" type="submit" text='Create' />
+                    </div>
+                </Form>
+            </div>
+        </div>
     )
 }
