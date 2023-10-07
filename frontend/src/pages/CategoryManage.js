596900155e4fa5b42aafa8c8c6386bae02117dbf
TrinhDuc01 <ductrinh.0301@gmail.com>
2023-10-07 22:10:22 +0700

    Category page kick user is not admin

--- frontend/src/pages/CategoryManage.js
+++ frontend/src/pages/CategoryManage.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router'
+import jwt_decode from "jwt-decode";
+
 
 export default function CategoryManage() {
-  return (
-    <div>CategoryManage</div>
-  )
+    
+    const naviagte = useNavigate();
+    const user = useSelector(state => state.auth.login.currentUser);
+    const decode = jwt_decode(user?.accessToken)// giai ma lay payload cua access token
+    
+    useEffect(() => {
+        if (!decode.isadmin) naviagte('/') // khong phai admin thi ve trang chu
+    }, [])
+    
+    return (
+        <div>CategoryManage</div>
+    )
 }
