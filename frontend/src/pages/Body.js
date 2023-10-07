6652de37d073500b3fc3090bc3cf9d7c506bc4fd
TrinhDuc01 <ductrinh.0301@gmail.com>
2023-10-07 20:28:44 +0700

    .

--- frontend/src/pages/Body.js
+++ frontend/src/pages/Body.js
@@ -12,7 +12,7 @@ export default function Body() {
         <div className='row'>
             <div className='text-center'>
                 {toggle ? <Home className='col-12' height='auto' width='auto' video={video} />
-                    : <Button type='button' className='btn btn-primary text-center mt-5 col-5 col-xl-3' text='Điều bất ngờ!!' handleClickRoll={handleClickRoll} />}
+                    : <Button type='button' className='btn btn-primary text-center mt-5 col-5 col-xl-3' text='Điều bất ngờ!!' handleClick={handleClickRoll} />}
             </div>
 
         </div>
