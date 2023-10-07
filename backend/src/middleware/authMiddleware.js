07e2465eab8e34980d4b89d58320ae4635611900
TrinhDuc01 <ductrinh.0301@gmail.com>
2023-10-08 00:37:18 +0700

    add response message

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -13,31 +13,43 @@ const authMiddleware = {
             const accessToken = token.split(" ")[1];
             try {
                 const user = tokenService.verifyToken(accessToken, process.env.ACCESS_SECRET_KEY);
-                if (!user) return res.status(401).json("You are not authenticated");
+                if (!user) return res.status(401).json({
+                    message: "You are not authenticated"
+                });
                 return next();
             } catch (error) {
-                return res.status(403).json("Token is not valid");
+                return res.status(403).json({
+                    message: "Token is not valid"
+                });
             }
         }
-        return res.status(401).json("You are not authenticated");
+        return res.status(401).json({
+            message: "You are not authenticated",
+        });
     },
 
     verifyTokenAndAdminAuth: (req, res, next) => {
         const token = req.headers.token;
-        // console.log(token)
+        console.log(token)
         if (token) {
             const accessToken = token.split(" ")[1];
             try {
                 const user = tokenService.verifyToken(accessToken, process.env.ACCESS_SECRET_KEY);
                 console.log(user)
-                if (!user) return res.status(401).json("You are not authenticated");
+                if (!user) return res.status(401).json({
+                    message: "You are not authenticated",
+                });
                 if (user.isadmin === 1) return next();
-                return res.status(401).json("You are not authenticated");
+                return res.status(401).json({ message: "You are not admin" });
             } catch (error) {
-                return res.status(403).json("Token is not valid");
+                return res.status(403).json({
+                    message: "Token is not valid",
+                });
             }
         }
-        return res.status(401).json("You are not authenticated");
+        return res.status(401).json({
+            message: "You are not authenticated",
+        });
     }
 }
 
