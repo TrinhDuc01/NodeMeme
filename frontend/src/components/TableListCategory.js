5446965cfbfe5866bf1c2985ba1e956ff8c0da40
TrinhDuc01 <ductrinh.0301@gmail.com>
2023-10-17 22:14:51 +0700

    fix

--- frontend/src/components/TableListCategory.js
+++ frontend/src/components/TableListCategory.js
@@ -1,19 +1,18 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux';
-import { getAllCategory } from '../api/apiCategoryRequest';
-import { RiDeleteBin6Line } from "react-icons/ri";
+import { useDispatch, useSelector } from 'react-redux';
+import { getCategoryUpdate } from '../api/apiCategoryRequest';
 import { RxUpdate } from "react-icons/rx";
 import { AiOutlineCheck, AiOutlineClose } from "react-icons/ai";
 
-export default function TableListCategory({ currentListCategory }) {
+export default function TableListCategory({ currentListCategory, axiosRefeshToken }) {
     //lay state tu kho chua
-    
+    let accessToken = useSelector(state => state.auth.login.currentUser?.accessToken)
     const dispatch = useDispatch();
     //lay all category
-    useEffect(() => {
-        // console.log('cl')
-        getAllCategory(dispatch);
-    }, [dispatch]);
+    
+
+    const handleUpdateCategory = (id) => {
+        getCategoryUpdate(dispatch, accessToken, id, axiosRefeshToken)
+    }
 
     return (
         <table className="table table-hover rounded text-center">
@@ -22,7 +21,7 @@ export default function TableListCategory({ currentListCategory }) {
                     <th scope="col">STT</th>
                     <th scope="col">Name</th>
                     <th scope="col">IsActive</th>
-                    <th scope="col">Thao tác</th>
+                    <th scope="col">Action</th>
                 </tr>
             </thead>
             <tbody>
@@ -30,10 +29,9 @@ export default function TableListCategory({ currentListCategory }) {
                     <tr key={category.id}>
                         <th scope="row">{index + 1}</th>
                         <td>{category.name}</td>
-                        <td className='text-center'>{category.isActive ? <AiOutlineCheck /> : <AiOutlineClose />}</td>
+                        <td className='text-center'>{category.isActive ? <AiOutlineCheck color='green' /> : <AiOutlineClose color='red' />}</td>
                         <td>
-                            <button className='btn btn-danger py-auto'><RiDeleteBin6Line /></button>
-                            <button className='btn btn-primary my-auto'><RxUpdate /></button>
+                            <button onClick={() => handleUpdateCategory(category.id)} className='btn btn-primary my-auto'><RxUpdate /></button>
                         </td>
                     </tr>
                 ))}
--- frontend/src/pages/CategoryManage.js
+++ frontend/src/pages/CategoryManage.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router'
 import Form from '../components/Form';
 import Input from '../components/Input';
 import Button from '../components/Button';
@@ -8,16 +7,21 @@ import { createCategory, getAllCategory } from '../api/apiCategoryRequest';
 import { createAxiosRefreshToken } from '../api/apiRefreshToken';
 import { loginSuccess } from '../redux/authSlice';
 import PaginateItem from '../components/PaginateItemCategory';
+import UpdateCategoryModal from '../components/UpdateCategoryModal';
+
 
 
 export default function CategoryManage() {
     const user = useSelector(state => state.auth.login.currentUser);
     const categoryList = useSelector(state => state.category.listCategory);
     const [categoryName, setCategoryName] = useState('')
-    const navigate = useNavigate();
     const dispatch = useDispatch();
     let axiosRefeshToken = createAxiosRefreshToken(user?.accessToken, dispatch, loginSuccess); // tra ve 1 axios kiem tra access token
     // console.log(user?.accessToken)
+
+    useEffect(() => {
+        getAllCategory(dispatch);
+    }, []);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -25,19 +29,16 @@ export default function CategoryManage() {
             name: categoryName,
             isActive: 1
         }
-
-        createCategory(category, navigate, user?.accessToken, axiosRefeshToken);//tao 1 category 
-        getAllCategory(dispatch);
-        setCategoryName('')
+        createCategory(category, user?.accessToken, axiosRefeshToken, dispatch);//tao 1 category 
+        setCategoryName('');
     }
-
-
 
 
     return (
         <div className='row mt-lg-1 d-block d-lg-flex gap-2'>
+            <UpdateCategoryModal axiosRefeshToken={axiosRefeshToken} />
             <div className='col-3'></div>
-            <div className='col-lg-6 mt-lg-1'>
+            <div className='col-lg-6 my-1'>
                 <Form handleSearchSubmit={handleSubmit} className='d-flex gap-2'>
                     <Input handleOnchange={e => setCategoryName(e.target.value)} value={categoryName}
                         className='form-control me-2'
@@ -45,7 +46,7 @@ export default function CategoryManage() {
                     <Button className="btn btn-primary" type="submit" text='Create' />
                 </Form>
             </div>
-            <PaginateItem items={categoryList} itemsPerPage={7} />
+            <PaginateItem items={categoryList} itemsPerPage={7} axiosRefeshToken={axiosRefeshToken} />
         </div>
     )
 }
